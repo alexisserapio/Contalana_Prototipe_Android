3600bc8720003a52b66ec2e198132d925df3a480
Alexis Serapio <alexis.serapioh@gmail.com>
2025-09-09 19:54:05 -0600

    Se añade la primer pantalla de bienvenida

---
+++ app/src/main/java/com/alexisserapio/contalana_prototipe/controller/welcomeActivity.kt
@@ -0,0 +1,53 @@
+package com.alexisserapio.contalana_prototipe.controller
+
+import android.content.Intent
+import android.graphics.Matrix
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.alexisserapio.contalana_prototipe.R
+import com.alexisserapio.contalana_prototipe.databinding.ActivityWelcomeBinding
+
+class welcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        //setContentView(R.layout.activity_welcome)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //Setear la matriz de la imagen para utilizarla en el Style
+        binding.welcomeImage.post {
+            val matrix = Matrix()
+
+            val drawable = binding.welcomeImage.drawable ?: return@post
+
+            // Escalar para que la imagen quepa en el ImageView
+            val scaleX = binding.welcomeImage.width.toFloat() / drawable.intrinsicWidth
+            val scaleY = binding.welcomeImage.height.toFloat() / drawable.intrinsicHeight
+            matrix.setScale(scaleX, scaleY)
+
+
+            binding.welcomeImage.imageMatrix = matrix
+        }
+
+        binding.welcomeButton.setOnClickListener {
+            val segueToInformativeAct = Intent(this, informativeActivity::class.java)
+            startActivity(segueToInformativeAct)
+            finish()
+        }
+
+    }
+}
